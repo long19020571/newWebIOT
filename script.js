2fea66a2bb52c7c108652a88ca6b0303f45fb5b8
long19020571 <125978668+long19020571@users.noreply.github.com>
2025-02-14 03:43:14 +0700

    Update script.js
    aaa

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ onValue(dataRef, (snapshot) => {
     if (data) {
         allTimestamps = Object.keys(data);
         allValues = Object.values(data).filter(v => !isNaN(v)).map(Number);
+      console.log("📊 Dữ liệu allValues sau khi xử lý:", allValues);
         updateChart();
     }
 });
